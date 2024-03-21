9c0280c013e5b884b4cb0f36f1058a592d09e59c
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2024-03-21 10:13:59 +0100

    chore(popup): Connect to a quote api

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,18 @@
+const getQuote = async () => {
+	try {
+		const res = await fetch("https://zenquotes.io/api/random");
+		const data = await res.json();
+		return data[0]["q"];
+	} catch (error) {
+		return "No quote to show";
+	}
+};
+
 document.addEventListener("DOMContentLoaded", function () {
 	var helloButton = document.getElementById("helloButton");
 
-	helloButton.addEventListener("click", function () {
-		alert("Hello world");
+	helloButton.addEventListener("click", async function () {
+		const quote = await getQuote();
+		alert(quote);
 	});
 });
